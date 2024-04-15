01ddd61d44b7a908c32866fafb05c6bdebb0f6ec
Rafael Santos Carvalho <rafasant@c1r16s3.42lisboa.com>
2024-04-15 18:13:49 +0100

    to Mac

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,16 +6,11 @@
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 03:48:29 by rafasant          #+#    #+#             */
-/*   Updated: 2024/04/14 03:52:46 by rafasant         ###   ########.fr       */
+/*   Updated: 2024/04/15 17:12:07 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	f(void *)
-{
-	return ;
-}
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
@@ -28,31 +23,31 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	}
 }
 
-int	main(void)
-{
-	t_list	*lst;
-	t_list	*temp;
-	int		i;
+// int	main(void)
+// {
+// 	t_list	*lst;
+// 	t_list	*temp;
+// 	int		i;
 
-	lst = ft_lstnew((void*)0);
-	i = 0;
-	while (i < 5)
-	{
-		ft_lstadd_back(&lst, ft_lstnew((void*)1 + i));
-		i++;
-	}
-	temp = lst;
-	while (temp != NULL)
-	{
-		printf("Before: %lu\n", (unsigned long int)temp->content);
-		temp = temp->next;
-		i++;
-	}
-	ft_lstiter(lst, f);
-	while (lst != NULL)
-	{
-		printf("After: %lu\n", (unsigned long int)lst->content);
-		lst = lst->next;
-		i++;
-	}
-}
+// 	lst = ft_lstnew((void*)0);
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		ft_lstadd_back(&lst, ft_lstnew((void*)1 + i));
+// 		i++;
+// 	}
+// 	temp = lst;
+// 	while (temp != NULL)
+// 	{
+// 		printf("Before: %lu\n", (unsigned long int)temp->content);
+// 		temp = temp->next;
+// 		i++;
+// 	}
+// 	ft_lstiter(lst, f);
+// 	while (lst != NULL)
+// 	{
+// 		printf("After: %lu\n", (unsigned long int)lst->content);
+// 		lst = lst->next;
+// 		i++;
+// 	}
+// }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,24 +6,72 @@
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 04:02:41 by rafasant          #+#    #+#             */
-/*   Updated: 2024/04/14 04:10:28 by rafasant         ###   ########.fr       */
+/*   Updated: 2024/04/15 17:22:46 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	*f(void *content)
+{
+	return (content + 1);
+}
+
+void	del(void *content)
+{
+	(void)content;
+	return ;
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	int	size;
+	t_list	*temp;
+	t_list	*new_node;
 
-	size = ft_lstsize(lst);
-	new_lst = ft_lstnew(f(lst->content));
-	while (size >= 0)
+	new_lst = ft_lstnew(lst->content);
+	temp = lst->next;
+	while (temp != NULL)
 	{
-		lst = lst->next;
-		ft_lstadd_back(new_lst, ft_lstnew(f(lst->content)));
-		size--;
+		new_node = ft_lstnew(f(temp->content + 1));
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		temp = temp->next;
 	}
 	return (new_lst);
 }
+
+int	main(void)
+{
+	t_list	*lst;
+	t_list	*temp;
+	int		i;
+
+	lst = ft_lstnew((void*)0);
+	i = 0;
+	while (i < 5)
+	{
+		ft_lstadd_back(&lst, ft_lstnew((void*)1 + i));
+		i++;
+	}
+	temp = lst;
+	i = 0;
+	while (temp != NULL)
+	{
+		printf("Before: %lu\n", (unsigned long int)temp->content);
+		temp = temp->next;
+		i++;
+	}
+	t_list	*new_lst;
+	new_lst = ft_lstmap(lst, f, del);
+	while (new_lst != NULL)
+	{
+		printf("After: %lu\n", (unsigned long int)new_lst->content);
+			new_lst = new_lst->next;
+		i++;
+	}
+} //porque e que o primeiro elemento da lista nova nao fica com 1? como e que posso passar o valor 0 para dentro da funcao f para que saia 1
