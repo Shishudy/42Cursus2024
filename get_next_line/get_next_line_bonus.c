89a33c1bdbbb9bd225d9eb2589c039fc868f67bb
Rafael Santos Carvalho <rafasant@c1r16s3.42lisboa.com>
2024-06-16 17:59:52 +0100

    fix .h and add bonus

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/15 15:21:37 by rafasant          #+#    #+#             */
-/*   Updated: 2024/06/16 17:52:28 by rafasant         ###   ########.fr       */
+/*   Created: 2024/06/16 17:07:40 by rafasant          #+#    #+#             */
+/*   Updated: 2024/06/16 17:51:37 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 char	*get_next_line(int fd)
 {
 	char		*str;
-	static char	buffer[BUFFER_SIZE];
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE];
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1 || fd > FOPEN_MAX)
 		return (NULL);
-	if (buffer[0] == '\0')
+	if (buffer[fd][0] == '\0')
 	{
-		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+		if (read(fd, buffer[fd], BUFFER_SIZE) <= 0)
 			return (NULL);
 	}
-	str = get_str(fd, buffer, NULL);
+	str = get_str(fd, buffer[fd], NULL);
 	if (!str)
 		return (NULL);
 	return (str);
