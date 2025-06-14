1ecef1e0e0eacf485fbca6f510eabeacfa48b580
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-06-14 18:40:54 +0100

    14/06/2025

--- Philosophers/philo/philosophers.h
+++ Philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 19:01:41 by rafasant          #+#    #+#             */
-/*   Updated: 2025/06/13 17:25:33 by rafasant         ###   ########.fr       */
+/*   Updated: 2025/06/14 18:15:39 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,27 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+# define GRAB 1
+# define EATING 2
+# define SLEEPING 3
+# define THINKING 4
+# define DEAD 5
+
 typedef struct s_catch
 {
 	char		*error_msg;
 }				t_catch;
+
+typedef struct s_sphilo
+{
+	int				philo_id;
+	time_t			internal_timer;
+	time_t			started_eating;
+	time_t			started_sleeping;
+	pthread_mutex_t	fork1;
+	pthread_mutex_t	*fork2;
+	pthread_t		philo;
+}				t_sphilo;
 
 typedef struct s_philo
 {
@@ -31,7 +48,7 @@ typedef struct s_philo
 	int			time_to_eat;
 	int			time_to_sleep;
 	int			number_of_times_each_philosopher_must_eat;
-	pthread_t	*arr_philos;
+	t_sphilo	*arr_philos;
 	time_t		start_time;
 }				t_philo;
 
--- Philosophers/philo/start_philos.c
+++ Philosophers/philo/start_philos.c
@@ -6,7 +6,7 @@
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:23:01 by rafasant          #+#    #+#             */
-/*   Updated: 2025/06/13 17:54:27 by rafasant         ###   ########.fr       */
+/*   Updated: 2025/06/14 18:40:33 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,68 @@
 
 pthread_mutex_t	mutex;
 
-void	print_msg(int i, char *msg)
+void	print_msg(int i, int action)
 {
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	printf("%ld %d %s\n", (tv.tv_sec - (philos()->start_time)) * 1000, i, msg);
+	if (action == GRAB)
+		printf("%ld %d has taken a fork\n", \
+			(tv.tv_sec - (philos()->start_time)) * 1000, i);
+	else if (action == EATING)
+		printf("%ld %d is eating\n", \
+			(tv.tv_sec - (philos()->start_time)) * 1000, i);
+	else if (action == SLEEPING)
+		printf("%ld %d is sleeping\n", \
+			(tv.tv_sec - (philos()->start_time)) * 1000, i);
+	else if (action == THINKING)
+		printf("%ld %d is thinking\n", \
+			(tv.tv_sec - (philos()->start_time)) * 1000, i);
+	else if (action == DEAD)
+		printf("%ld %d died\n", \
+			(tv.tv_sec - (philos()->start_time)) * 1000, i);
+
 }
 
-void	*philo_eating(void *arg)
+void	*eat_sleep_think(void *arg)
 {
-	print_msg(*(int *)arg, "is eating");
-	usleep(philos()->time_to_eat);
-	return NULL;
-}
+	t_sphilo	*sphilo;
+	time_t		result;
+	time_t		after_eating;
+	time_t		before_eating;
+	struct timeval	tv;
 
-void	*philo_thinking(void *arg)
-{
-	print_msg(*(int *)arg, "is thinking");
-	return NULL;
-}
+	sphilo = (t_sphilo *)arg;
+	while (1)
+	{
+		pthread_mutex_lock(&sphilo->fork1);
+		print_msg(sphilo->philo_id, GRAB);
+		pthread_mutex_lock(&sphilo->fork2);
+		print_msg(sphilo->philo_id, GRAB);
+		print_msg(sphilo->philo_id, EATING);
+		sphilo->internal_timer = 0;
+		result = sphilo->internal_timer + philos()->time_to_eat; //tenho de ajustar para o tipo de varivel de tmepos bater certo, o valor resltante de gettimeofday e time_to_die sao diffs
+		if (result > sphilo->internal_timer + philos()->time_to_die)
+		{
+			usleep(result);
+			print_msg(sphilo->philo_id, DEAD);
+			pthread_mutex_unlock(&sphilo->fork1);
+			pthread_mutex_unlock(&sphilo->fork2);
+			return ;
+		}
+		else
+			usleep(philos()->time_to_eat);
+		pthread_mutex_unlock(&sphilo->fork1);
+		pthread_mutex_unlock(&sphilo->fork2);
+		print_msg(sphilo->philo_id, SLEEPING);
+		sphilo->started_sleeping = gettimeofday(&tv, NULL);
+		if (philos()->time_to_die < philos()->time_to_eat)
+			usleep(philos()->time_to_sleep);
+		print_msg(sphilo->philo_id, THINKING);
 
-void	*philo_sleeping(void *arg)
-{
-	print_msg(*(int *)arg, "is sleeping");
-	usleep(philos()->time_to_sleep);
-	return NULL;
+
+
+	}
 }
 
 void	sit_philos()
@@ -50,17 +86,9 @@ void	sit_philos()
 
 	i = 0;
 	while (i < philos()->number_of_philosophers)
-	{
-		if (i % 2 == 0 && i + 1 != philos()->number_of_philosophers)
-		{
-			if (pthread_create(&philos()->arr_philos[i], NULL, &philo_eating, &i) != 0)
-				return ((void) (catch()->error_msg = "Thread creation failure!"));
-		}
-		else
-		{
-			if (pthread_create(&philos()->arr_philos[i], NULL, &philo_thinking, &i) != 0)
-				return ((void) (catch()->error_msg = "Thread creation failure!"));
-		}
+	{ 
+		if (pthread_create(&philos()->arr_philos[i].philo, NULL, &eat_sleep_think, &philos()->arr_philos[i]) != 0)
+			return ((void) (catch()->error_msg = "Thread creation failure!"));
 		i++;
 	}
 	i = 0;
