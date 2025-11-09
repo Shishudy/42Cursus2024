cb0558afe85b0bc7e643db271f14224d70f48734
Rafael <rafael.carvalh2012@gmail.com>
2025-11-09 02:09:44 +0000

    cpp00, ex00

---
+++ CPP/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 00:42:46 by rafasant          #+#    #+#             */
+/*   Updated: 2025/11/09 01:59:12 by rafasant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			argv[i][j] = (char)toupper(argv[i][j]);
+			j++;
+		}
+		std::cout << argv[i];
+		i++;
+	}
+	std::cout << '\n';
+	return (0);
+}
