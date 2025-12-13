840ae8dcb48df5eed8e047a4b7412242fa0916da
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-12-13 22:59:50 +0000

    started CPP01

---
+++ CPP/CPP01/ex00/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 20:52:13 by rafasant          #+#    #+#             */
+/*   Updated: 2025/12/13 20:55:29 by rafasant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+	public:
+		Contact(void);
+		void CreateContact(void);
+		void ClearContact(void);
+		std::string GetFirstName(void);
+		std::string GetLastName(void);
+		std::string GetNickname(void);
+		std::string GetPhoneNumber(void);
+		std::string GetDarkestSecret(void);
+		~Contact(void);
+
+	private:
+		std::string	_name;
+};
+
+#endif
